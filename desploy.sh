e8500a89f24f57a1915a5500a5b0162a42fcd959
Chan Kim <ckim0419@gmail.com>
2016-11-17 13:49:58 -0500

    added requirement check.

--- desploy.sh
+++ desploy.sh
@@ -2,12 +2,20 @@
 
 set -e
 
-install -d $HOME/.vim/bundle
-install -d $HOME/.vim/colors
+
+# check requirement
+for req in go python-config git cmake ; 
+  which $req &>/dev/null || echo "Requirement $req is not met." && exit 1
+done
+
+install -d $HOME/.vim/{colors,bundle,autoload}
 install vimrc $HOME/.vimrc
 install ctags $HOME/.ctags
 install tmux.conf $HOME/.tmux.conf
 install molokai.vim $HOME/.vim/colors/molokai.vim
+
+# Install PATH
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 PROJECT_BASE=$PWD
 pushd $HOME/.vim/bundle
